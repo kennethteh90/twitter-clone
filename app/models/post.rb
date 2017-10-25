3d07beb7c9db52bf7b700d9fd7ccf70177b2df6f
kennethteh90 <kennethteh90@gmail.com>
2017-10-25 10:16:06 +0800

    fixed css classes, minor fix to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
 
-  has_many :post_tags
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates :content, presence: true, length: { maximum: 140 }
