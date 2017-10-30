041dd95ab53ae90fba1b0cc7f37a2c1ee4eb9107
kennethteh90 <kennethteh90@gmail.com>
2017-10-30 12:11:04 +0800

    modified circle.yml file

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,10 +32,10 @@ class User < ApplicationRecord
 
   # Returns a user's status feed.
   def feed
-  following_ids = "SELECT followed_id FROM relationships
-                   WHERE  follower_id = :user_id"
-  Post.where("user_id IN (#{following_ids})
-                   OR user_id = :user_id", user_id: id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
 end
