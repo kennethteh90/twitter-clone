08de525f148f3fa37266112b71cfc0d058347ce3
kennethteh90 <kennethteh90@gmail.com>
2017-11-01 15:38:04 +0800

    add some controller specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe PostsController, type: :controller do
     # how to test feed items?
     it { expect(assigns(:tag)).to be_a(Tag) }
 
+  end
+
+  describe 'DELETE #destroy' do
+
+    let!(:post) { create(:post, user: user) }
+
+    before { delete :destroy, params: { id: post } }
+
+    it { expect(assigns(:post)).to be_destroyed }
+    it { is_expected.to set_flash[:deleted] }
+    it { is_expected.to redirect_to(profile_users_all_index_path) }
 
   end
 
