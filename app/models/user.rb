75822792cd56b2f757bd71ae2304cb18eb3a2973
kennethteh90 <kennethteh90@gmail.com>
2017-11-07 14:41:41 +0800

    added facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :posts
   has_many :active_relationships, class_name:  "Relationship",
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   # Follows a user.
   def follow(other_user)
-    following << other_user
+    following << other_user if other_user != self && !following?(other_user)
   end
 
   # Unfollows a user.
@@ -39,6 +39,18 @@ class User < ApplicationRecord
                      WHERE  follower_id = :user_id"
     Post.where("user_id IN (#{following_ids})
                      OR user_id = :user_id", user_id: id)
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
 
 end
